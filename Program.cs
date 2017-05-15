c4dc878c67d1cd2e42fca3ac8ba4faf3e5ec9e5f
samvelie <sam.velie@gmail.com>
2017-05-14 22:42:02 -0500

    instatiated classes from abstract

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace haha
     {
         static void Main(string[] args)
         {
+            Lion lion = new Lion(Lion.ColorSpectrum.White); 
+            lion.Hunt(); 
+            lion.Eat(); 
+            lion.Sleep(); 
+
+            Tiger tiger = new Tiger(Tiger.ColorSpectrum.Blue); 
+            tiger.Hunt(); 
+            tiger.Eat(); 
+            tiger.Sleep(); 
+
+            ReadLine();
         }
     }
 
@@ -17,19 +28,48 @@ namespace haha
     }
     public class Lion : Cat
     {
+        public enum ColorSpectrum {Brown, White}
+        public string LionColor {get; set;}
+        public Lion(ColorSpectrum color)
+        {
+            LionColor = color.ToString();
+        }
         public override void Eat()
         {
-            throw new NotImplementedException();
+            WriteLine($"The {LionColor} lion eats.");
         }
 
         public override void Hunt()
         {
-            throw new NotImplementedException();
+            WriteLine($"The {LionColor} lion sleeps.");
         }
 
         public override void Sleep()
         {
-            throw new NotImplementedException();
+            WriteLine($"The {LionColor} lion sleeps.");
+        }
+    }
+    public class Tiger : Cat
+    {
+        public enum ColorSpectrum {Orange, White, Gold, Blue, Black}
+        public string TigerColor {get; set;}
+        public Tiger(ColorSpectrum color)
+        {
+            TigerColor = color.ToString();
+        }
+        public override void Eat()
+        {
+            WriteLine($"The {TigerColor} lion eats.");
+        }
+
+        public override void Hunt()
+        {
+            WriteLine($"The {TigerColor} lion sleeps.");
+        }
+
+        public override void Sleep()
+        {
+            WriteLine($"The {TigerColor} lion sleeps.");
         }
     }
 }
