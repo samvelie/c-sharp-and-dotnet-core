d5e7bb4d694af32822b4aabf0bec92f60876b9f1
samvelie <sam.velie@gmail.com>
2017-05-14 22:27:37 -0500

    learning abstract classes:

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+namespace haha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Cat
+    {
+        public abstract void Eat();
+        public abstract void Hunt();
+        public abstract void Sleep();
+    }
+    public class Lion : Cat
+    {
+        public override void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Hunt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Sleep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
